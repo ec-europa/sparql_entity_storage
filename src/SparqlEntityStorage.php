7058c0b2042fad10c18c54c94c0cbbdb9621c8b6
Claudiu Cristea <clau.cristea@gmail.com>
2021-02-03 19:26:49 +0200

    ISAICP-3576: Remove unused variable.

--- src/SparqlEntityStorage.php
+++ src/SparqlEntityStorage.php
@@ -451,7 +451,6 @@ QUERY;
     $values_per_entity = [];
     foreach ($results as $result) {
       $entity_id = (string) $result->entity_id;
-      $entity_graphs[$entity_id] = (string) $result->graph;
 
       $lang = LanguageInterface::LANGCODE_DEFAULT;
       if ($result->field_value instanceof Literal) {
