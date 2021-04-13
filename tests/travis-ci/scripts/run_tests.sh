0006253f6afa685b10d78f21462e2cd593759d10
Claudiu Cristea <clau.cristea@gmail.com>
2021-04-13 11:56:09 +0300

    ISAICP-6483: Simplify more.

--- tests/travis-ci/scripts/run_tests.sh
+++ tests/travis-ci/scripts/run_tests.sh
@@ -1,50 +1,35 @@
 #!/bin/bash
 
 install_codebase () {
-    mkdir -p "${SITE_DIR}/web/modules"
-    cp "${TRAVIS_BUILD_DIR}/tests/travis-ci/fixtures/composer.json.dist" "${SITE_DIR}/composer.json"
-    perl -i -pe's/\$\{([^}]+)\}/$ENV{$1}/' "${SITE_DIR}/composer.json"
-    ln -s "${TRAVIS_BUILD_DIR}" "${SITE_DIR}/web/modules/sparql_entity_storage"
-    cd "${SITE_DIR}" && COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --prefer-dist
+    mkdir -p "${SITE_DIR}/modules"
+    ln -s "${TRAVIS_BUILD_DIR}" "${SITE_DIR}/modules/sparql_entity_storage"
+    composer require --dev drupal/core-recommended:${DRUPAL} drupal/core-composer-scaffold:${DRUPAL} phpunit/phpunit:${PHPUNIT} --no-interaction --prefer-dist
 }
 
-case "${TEST}" in
+set -x
+
+case "${TEST_SUITE}" in
     PHPCodeSniffer)
-        cd "${TRAVIS_BUILD_DIR}"
         composer install
         ./vendor/bin/phpcs
         exit $?
         ;;
     PHPStan)
-        # Deploy the codebase.
         install_codebase
-        cp "${TRAVIS_BUILD_DIR}/tests/travis-ci/fixtures/phpstan.neon.dist" "${SITE_DIR}/phpstan.neon"
-
-        # Run static analysis.
-        ./vendor/bin/phpstan analyse ./web/modules/sparql_entity_storage
+        ./vendor/bin/phpstan analyse ../testing_site/modules/sparql_entity_storage
         exit $?
         ;;
     PHPUnit)
-        set -x
-        # Deploy the codebase.
         install_codebase
-
-        # Setup PHPUnit.
-        cp "${TRAVIS_BUILD_DIR}/tests/travis-ci/fixtures/phpunit.xml.dist" "${SITE_DIR}/phpunit.xml"
-
         # Virtuoso setup.
         mkdir "${SITE_DIR}/virtuoso"
         docker run --name virtuoso -p 8890:8890 -p 1111:1111 -e SPARQL_UPDATE=true -v "${SITE_DIR}/virtuoso":/data -d tenforce/virtuoso
-
-        # Sleep to ensure that docker services are available.
+        # Sleep to ensure that Docker services are available.
         sleep 15
-
-        # Run PHPUnit.
-        cd "${SITE_DIR}"
         ./vendor/bin/phpunit --verbose
         exit $?
         ;;
     *)
-        echo "Unknown test '$1'"
+        echo "Unknown test '${TEST_SUITE}'"
         exit 1
 esac
